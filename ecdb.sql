3589e6b4eb1841f9fa4b5be6e5952e6378229187
skynet35 <45698752+skynet35@users.noreply.github.com>
2022-01-10 15:16:49 +0100

    Update ecdb.sql

--- ecdb.sql
+++ ecdb.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
+-- version 4.0.10.20
+-- https://www.phpmyadmin.net
 --
--- Värd: localhost
--- Skapad: 04 nov 2012 kl 15:17
--- Serverversion: 5.5.16
--- PHP-version: 5.2.17
+-- Client: localhost
+-- Généré le: Mer 22 Décembre 2021 à 23:18
+-- Version du serveur: 5.1.73
+-- Version de PHP: 5.6.40
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databas: `ecdb`
+-- Base de données: `ecdb_database`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `category_head`
+-- Structure de la table `category_head`
 --
 
 CREATE TABLE IF NOT EXISTS `category_head` (
@@ -32,32 +32,33 @@ CREATE TABLE IF NOT EXISTS `category_head` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Dumpning av Data i tabell `category_head`
+-- Contenu de la table `category_head`
 --
 
 INSERT INTO `category_head` (`id`, `name`) VALUES
-(1, 'Cable'),
-(2, 'Capacitor'),
-(3, 'Connector'),
+(1, 'Cables'),
+(2, 'Condensateurs'),
+(3, 'Connecteurs'),
 (4, 'Diode'),
-(5, 'IC'),
-(6, 'Inductor'),
-(7, 'Mechanic'),
-(16, 'Module'),
+(5, 'CI'),
+(6, 'Inductances'),
+(7, 'Mechanique'),
+(16, 'Modules'),
 (8, 'Opto'),
-(18, 'Oscillator'),
-(13, 'Resistor'),
-(15, 'Sensor'),
-(10, 'Switch'),
-(11, 'Transformer'),
-(12, 'Transistor'),
-(14, 'Display'),
-(17, 'Miscellaneous');
+(9, 'Protections'),
+(18, 'Oscillateurs'),
+(13, 'Resistances'),
+(15, 'Capteurs'),
+(10, 'Interrupteur/poussoirs'),
+(11, 'Régulateurs/Transfo'),
+(12, 'Transistors'),
+(14, 'Ecrans'),
+(17, 'Autres');
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `category_sub`
+-- Structure de la table `category_sub`
 --
 
 CREATE TABLE IF NOT EXISTS `category_sub` (
@@ -66,136 +67,140 @@ CREATE TABLE IF NOT EXISTS `category_sub` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Dumpning av Data i tabell `category_sub`
+-- Contenu de la table `category_sub`
 --
 
 INSERT INTO `category_sub` (`id`, `name`) VALUES
-(101, 'Ribbon'),
-(102, 'Coax'),
-(199, 'Misc'),
-(201, 'Ceramic'),
-(202, 'Electrolytic'),
+(101, 'Ruban'),
+(102, 'Coaxial'),
+(199, 'Divers'),
+(201, 'Ceramique'),
+(202, 'Electrolytique'),
 (203, 'Polyester'),
-(204, 'Tantalum'),
+(204, 'Tantal'),
 (205, 'Variable'),
-(299, 'Misc'),
+(299, 'Divers'),
 (301, 'Audio'),
-(302, 'Coax'),
-(303, 'DC'),
+(302, 'Coaxial'),
+(303, 'DC-alim'),
 (304, 'D-Sub'),
-(399, 'Misc'),
-(401, 'Rectifier'),
+(399, 'Divers'),
+(401, 'Redresseur'),
 (402, 'Schottky'),
-(403, 'Small Signal'),
+(403, 'Petits signaux'),
 (404, 'Zener'),
-(499, 'Misc'),
+(499, 'Divers'),
 (501, '4xxx'),
 (502, '74xx'),
 (503, 'Microcontroller'),
-(504, 'Comparator'),
-(505, 'Op. Amp.'),
+(504, 'Comparateur'),
+(505, 'AOP'),
 (506, 'Temperature'),
 (507, 'Timer & Osc.'),
-(508, 'Voltage Ref.'),
-(509, 'Voltage Reg.'),
-(599, 'Misc'),
+(508, 'Référence de tension'),
+(509, 'Régulateur de tension'),
+(599, 'Divers'),
 (601, 'Ferrite'),
-(602, 'Filter'),
-(603, 'Inductor'),
-(699, 'Misc'),
+(602, 'Filtre'),
+(603, 'Inducteur'),
+(699, 'Divers'),
 (701, 'Box'),
 (702, 'Distance'),
-(703, 'Fuse'),
-(704, 'Motor'),
-(705, 'Screw'),
-(799, 'Misc'),
-(801, 'Reflex coupler'),
+(703, 'Supports Fusibles'),
+(704, 'Moteurs'),
+(705, 'Relais'),
+(799, 'Divers'),
+(801, 'Barrières photo-electrique'),
 (802, 'Laser'),
 (803, 'LED'),
 (804, 'LED 3mm'),
 (805, 'LED 5mm'),
-(806, 'Optocoupler'),
-(899, 'Misc'),
-(901, 'Crystal'),
-(902, 'Resonator'),
-(999, 'Misc'),
-(1001, 'Keypad'),
-(1002, 'Momentary'),
-(1003, 'PCB Mounted'),
-(1004, 'Rotary Encoder'),
-(1005, 'Toggle Switch'),
-(1099, 'Misc'),
-(1101, 'Power Supply'),
-(1102, 'Transformer'),
-(1103, 'Wall Adapter'),
-(1199, 'Misc'),
-(1201, 'BJT'),
+(806, 'Optocoupleur'),
+(899, 'Divers'),
+(901, 'Fusibles'),
+(902, 'Varistances'),
+(903, 'Thermistances CTN'),
+(904, 'Thermistances CTP'),
+(905, 'Support Fusibles'),
+(999, 'Divers'),
+(1001, 'Clavier'),
+(1002, 'Momentanné'),
+(1003, 'Monté sur PCB'),
+(1004, 'Encodeur rotatif'),
+(1005, 'Maintient'),
+(1099, 'Divers'),
+(1101, 'Alimentation'),
+(1102, 'Transformateur'),
+(1103, 'Convertisseur DC/DC'),
+(1199, 'Divers'),
+(1201, 'JBT'),
 (1202, 'JFET'),
 (1203, 'MOSFET'),
 (1204, 'NPN'),
 (1205, 'PNP'),
-(1299, 'Misc'),
+(1207, 'Thyristor'),
+(1299, 'Divers'),
 (1301, '1/4W Carbon'),
 (1302, '1/4W Metal'),
 (1303, '1/6W Carbon'),
 (1304, '1/6W Metal'),
-(1305, '0603'),
-(1306, '0805'),
-(1307, '1206'),
+(1305, 'CMS-0603'),
+(1306, 'CMS-0805'),
+(1307, 'CMS-1206'),
 (1308, 'Effect'),
 (1309, 'Photo'),
-(1399, 'Misc'),
+(1399, 'Divers'),
 (1601, 'GSM'),
 (1602, 'GPS'),
-(1699, 'Misc'),
+(1699, 'Divers'),
 (1401, 'LCD'),
 (1402, 'VFD'),
 (1404, 'LED'),
 (1403, 'TFT'),
 (807, 'IR LED'),
-(1499, 'Misc'),
+(1499, 'Divers'),
 (708, 'IC Socket'),
-(709, 'Heat Sink'),
-(510, 'Data Converter'),
-(511, 'A/D Multiplexer'),
+(709, 'Radiateur'),
+(510, 'Convertisseur Data'),
+(511, 'A/D Multiplexeur'),
 (512, 'Driver'),
 (513, 'Opto Driver'),
-(514, 'DC/DC Converter'),
+(514, 'Convertisseur DC/DC'),
 (515, 'Audio/Video'),
-(516, 'Memory'),
+(516, 'Memoires'),
 (1311, 'Temperature'),
-(1310, 'Network'),
+(1310, 'Réseaux'),
 (305, 'HF'),
-(710, 'Knob'),
-(711, 'Meter'),
-(103, 'Standard'),
-(104, 'Mains'),
+(710, 'Bouton potar'),
+(711, 'Metre'),
+(103, 'Rouleau'),
+(104, 'Cable PC'),
 (105, 'Signal/Data'),
-(106, 'Fiber optic'),
+(106, 'Fibre optic'),
 (306, 'PCB'),
 (1603, 'Bluetooth'),
 (1604, 'WLAN'),
 (1605, 'ZigBee'),
 (1606, 'RFID'),
-(307, 'Mains'),
-(1501, 'Moisture'),
+(307, 'Cable PC'),
+(1501, 'Humidité'),
 (1502, 'Temperature'),
-(1503, 'Pressure'),
-(1504, 'Magnetic'),
-(1505, 'Hall Effect'),
-(1506, 'Gas'),
-(1507, 'Accelerometer'),
-(1508, 'Light'),
-(1509, 'Proximity'),
-(1599, 'Misc'),
-(1799, 'Misc'),
+(1503, 'Pression'),
+(1504, 'Magnetique'),
+(1505, 'Effet hall'),
+(1506, 'Gaz'),
+(1507, 'Accelerometre'),
+(1508, 'Lumières'),
+(1509, 'Proximité'),
+(1599, 'Divers'),
+(1798, 'Consommables'),
+(1799, 'Divers'),
 (517, 'Logic'),
-(1312, 'Potentiometer'),
-(1006, 'Relay'),
+(1312, 'Potentiometre'),
 (308, 'Data'),
 (1801, 'Crystal'),
-(1802, 'Resonator'),
-(1899, 'Misc'),
+(1802, 'Resonateur'),
+(1899, 'Divers'),
 (1007, 'DIP'),
 (406, 'Bridge'),
 (1206, 'Triac'),
@@ -206,7 +211,7 @@ INSERT INTO `category_sub` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `data`
+-- Structure de la table `data`
 --
 
 CREATE TABLE IF NOT EXISTS `data` (
@@ -236,10 +241,10 @@ CREATE TABLE IF NOT EXISTS `data` (
   `price` varchar(11) NOT NULL,
   KEY `Id` (`id`),
   KEY `owner` (`owner`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=517 ;
 
 --
--- Dumpning av Data i tabell `data`
+-- Contenu de la table `data`
 --
 
 INSERT INTO `data` (`id`, `owner`, `name`, `manufacturer`, `package`, `pins`, `smd`, `quantity`, `order_quantity`, `location`, `scrap`, `width`, `height`, `depth`, `weight`, `datasheet`, `comment`, `category`, `public`, `url1`, `url2`, `url3`, `url4`, `price`) VALUES
@@ -248,7 +253,7 @@ INSERT INTO `data` (`id`, `owner`, `name`, `manufacturer`, `package`, `pins`, `s
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `members`
+-- Structure de la table `members`
 --
 
 CREATE TABLE IF NOT EXISTS `members` (
@@ -263,19 +268,19 @@ CREATE TABLE IF NOT EXISTS `members` (
   `currency` varchar(3) NOT NULL DEFAULT 'USD',
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`member_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1800 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1804 ;
 
 --
--- Dumpning av Data i tabell `members`
+-- Contenu de la table `members`
 --
 
-INSERT INTO `members` (`member_id`, `firstname`, `lastname`, `login`, `mail`, `passwd`, `admin`, `measurement`, `currency`) VALUES
-(4, 'Demo', 'Demo', 'demo', 'mail@mailen.com', 'fe01ce2a7fbac8fafaed7c982a04e229', 0, 1, 'USD');
+INSERT INTO `members` (`member_id`, `firstname`, `lastname`, `login`, `mail`, `passwd`, `admin`, `measurement`, `currency`, `reg_date`) VALUES
+(4, 'Demo', 'Demo', 'demo', 'mail@mailen.com', 'fe01ce2a7fbac8fafaed7c982a04e229', 0, 1, 'USD', '2014-06-02 17:36:39'),
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `members_stats`
+-- Structure de la table `members_stats`
 --
 
 CREATE TABLE IF NOT EXISTS `members_stats` (
@@ -283,19 +288,18 @@ CREATE TABLE IF NOT EXISTS `members_stats` (
   `members_stats_member` int(11) NOT NULL,
   `members_stats_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`members_stats_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=258 ;
 
 --
--- Dumpning av Data i tabell `members_stats`
+-- Contenu de la table `members_stats`
 --
 
 INSERT INTO `members_stats` (`members_stats_id`, `members_stats_member`, `members_stats_time`) VALUES
 (1, 4, '2012-11-04 14:14:36');
-
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `projects`
+-- Structure de la table `projects`
 --
 
 CREATE TABLE IF NOT EXISTS `projects` (
@@ -304,10 +308,10 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `project_name` varchar(64) NOT NULL,
   PRIMARY KEY (`project_id`),
   KEY `project_owner` (`project_owner`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
--- Dumpning av Data i tabell `projects`
+-- Contenu de la table `projects`
 --
 
 INSERT INTO `projects` (`project_id`, `project_owner`, `project_name`) VALUES
@@ -316,7 +320,7 @@ INSERT INTO `projects` (`project_id`, `project_owner`, `project_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `projects_data`
+-- Structure de la table `projects_data`
 --
 
 CREATE TABLE IF NOT EXISTS `projects_data` (
@@ -329,10 +333,10 @@ CREATE TABLE IF NOT EXISTS `projects_data` (
   KEY `owner_id` (`projects_data_owner_id`),
   KEY `project_id` (`projects_data_project_id`),
   KEY `component_id` (`projects_data_component_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
 
 --
--- Dumpning av Data i tabell `projects_data`
+-- Contenu de la table `projects_data`
 --
 
 INSERT INTO `projects_data` (`projects_data_id`, `projects_data_owner_id`, `projects_data_project_id`, `projects_data_component_id`, `projects_data_quantity`) VALUES
