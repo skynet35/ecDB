476a689d322bab4b72db680e1a671a2e23951e25
skynet35 <45698752+skynet35@users.noreply.github.com>
2022-01-10 18:20:11 +0100

    Update header.php

--- include/header.php
+++ include/header.php
@@ -11,14 +11,14 @@
 			include('include/mysql_connect.php');
 			
 			$owner = $_SESSION['SESS_MEMBER_ID'];
-			$GetName = mysql_query("SELECT firstname, lastname FROM members WHERE member_id = ".$owner."");
-			$headername = mysql_fetch_assoc($GetName);
+			$GetName = mysqli_query($link, "SELECT firstname, lastname FROM members WHERE member_id = ".$owner."");
+			$headername = mysqli_fetch_assoc($GetName);
 			
 			if(isset($_POST['submit']) && $_SERVER["REQUEST_URI"] == '/ecdb/my.php') { echo $_POST['firstname']; } else { echo $headername['firstname']; }
 			echo ' ';
 			if(isset($_POST['submit']) && $_SERVER["REQUEST_URI"] == '/ecdb/my.php') { echo $_POST['lastname']; } else { echo $headername['lastname']; }
 		?>
-		</a> - <a href="logout.php"> Sign out</a>
+		</a> - <a href="logout.php"> Deconnexion</a>
 	</span>
 	
 	<div class="searchContent">
